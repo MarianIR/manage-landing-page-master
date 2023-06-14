11f8b8607ae949cae261a59ea987a4c93555975a
marianIR <marianraileanu37@gmail.com>
2023-06-14 12:15:12 +0300

    feat: update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "./Navbar";
+import Navbar from "../src/Navbar";
 import Hero from "./Hero";
 import Features from "./Features";
 import Testimonials from "./Testimonials";
